3a0e8f52f782e67c2a28b6bf8ce33079f560d1f3
Cedric Ziel <cedric@cedric-ziel.com>
2014-01-15 13:02:10 +0100

    [TASK] dokku bootstrapping on vagrant up

--- cookbooks/dokku/recipes/default.rb
+++ cookbooks/dokku/recipes/default.rb
@@ -6,6 +6,6 @@ execute 'fetch dokku' do
   command "wget -q -O /dokku_bootstrap.sh https://raw.github.com/progrium/dokku/master/bootstrap.sh"
 end
 
-#execute 'install dokku (this takes a while)' do
-#  command "bash /dokku_bootstrap.sh"
-#end
+execute 'install dokku (this takes a while)' do
+  command "sudo bash /dokku_bootstrap.sh"
+end
